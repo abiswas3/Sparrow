90949a8c8c8abb0ead3bb3b74bbc27a5099dd145
abiswas3 <aribiswas3@gmail.com>
2018-02-01 01:00:52 -0800

    api fixed

---
+++ jack.py
@@ -0,0 +1,50 @@
+import numpy as np
+from utils import *
+
+class Jack(object):
+
+    def __init__(self):
+
+        pass
+
+    def badify(self,
+               x,
+               gr,
+               eps = 0.1):
+
+        '''
+        Goodfellow badify
+        '''
+        eta = np.array([eps*sgn(i) for i in gr])
+
+        return x+eta
+
+    def uniform_noisify(self,
+                        x,
+                        eps = 0.5):
+
+        eta = eps*(np.random.uniform(-1,1, 784))
+
+        return x + eta
+
+    def turn_em_into_a_pirate(self,
+                              test_data,
+                              test_labels,
+                              model,
+                              eps=0.07,
+                              num_test_images = 'all'):
+
+        num_test_images = len(test_labels) if num_test_images == 'all' else num_test_images
+
+        xvals = test_data[:num_test_images]
+        lvals = test_labels[:num_test_images]
+
+        # Get gradient so i can update eta
+        print("Getting grads\n")
+        grvar = model.get_gradient(xvals, lvals)
+
+        adv = np.zeros(xvals.shape)
+        for i, xval in enumerate(xvals):
+            adv[i] = self.badify(xval, grvar[i], eps=eps)
+
+        return adv
--- model.py
+++ model.py
@@ -7,6 +7,7 @@ from sklearn.metrics import *
 # general purpose shit everythone should have
 from utils import *
 
+from jack import Jack
 
 class ImageClassifier(object):
 
@@ -16,85 +17,27 @@ class ImageClassifier(object):
 
         self.num_dimensions = num_dimensions
         self.num_classes    = num_classes
+        self.sess           = None
         self.build_model()
 
 
-    def badify(self,
-               x,
-               gr,
-               eps = 0.1):
 
-        '''
-        Goodfellow badify
-        '''
-        eta = np.array([eps*sgn(i) for i in gr])
+    def oracle(self, xvals):
 
-        return x+eta
+        if self.sess != None:
+            y_probs_noiseless = self.sess.run(self.y_hat, feed_dict={self.input_data: xvals})
+            return [np.argmax(i) for i in y_probs_noiseless]
+        else:
+            print("We have not learned a classifier")
+            return []
 
-    def uniform_noisify(self,
-                        x,
-                        eps = 0.5):
 
-        eta = eps*(np.random.uniform(-1,1, 784))
+    def get_gradient(self, xvals, lvals):
 
-        return x + eta
-
-    def evaluate(self,
-                 test_data,
-                 test_labels,
-                 session,
-                 eps=0.07,
-                 num_test_images = 'all'):
-
-        num_test_images = len(test_labels) if num_test_images == 'all' else num_test_images
-
-        xvals = test_data[:num_test_images]
-        lvals = test_labels[:num_test_images]
-        y_true = lvals[:]
-
-        # Get gradient so i can update eta
-        grvar = session.run(self.gv,
+        return  self.sess.run(self.gv,
                             feed_dict={self.input_data: xvals,
                                        self.input_labels: lvals})
-
-        y_probs_noiseless = session.run(self.y_hat, feed_dict={self.input_data: xvals})
-
-        adv = np.zeros(xvals.shape)
-        for i, xval in enumerate(xvals):
-            adv[i] = self.badify(xval, grvar[i], eps=eps)
-
-        y_probs_adv = session.run(self.y_hat, feed_dict={self.input_data: adv})
-
-        rand = np.zeros(xvals.shape)
-        for i, xval in enumerate(xvals):
-            rand[i] = self.uniform_noisify(xval, eps=eps)
-
-        y_probs_rand = session.run(self.y_hat, feed_dict={self.input_data: rand})
-
-        y_true_pred = [np.argmax(i) for i in y_true] 
-        y_noiseless_pred = [np.argmax(i) for i in y_probs_noiseless]
-        y_adv_pred = [np.argmax(i) for i in y_probs_adv]
-        y_rand_pred = [np.argmax(i) for i in y_probs_rand]
- 
-        s = "Noiseless {}{}{} Adversary {}{}{} Uniformly Random {}{}{}\n"
-        print(s.format(WARNING,
-                       accuracy_score(y_true_pred, y_noiseless_pred),
-                       ENDC,
-                       BLUE,
-                       accuracy_score(y_true_pred, y_adv_pred),
-                       ENDC,
-                       GREEN,
-                       accuracy_score(y_true_pred, y_rand_pred),
-                       ENDC))
-
-
-    def oracle(self):
-        pass
-
-    def get_gradient(self, func, variable):
-
-        return tf.gradients(func, [variable])[0]
-
+    
     def build_model(self):
 
         num_dimensions = self.num_dimensions
@@ -121,42 +64,51 @@ class ImageClassifier(object):
         self.optimizer = opter.minimize(self.loss)
 
         # function to get gradient value
-        self.gv = self.get_gradient(self.loss, self.input_data)
+        self.gv = tf.gradients(self.loss, [self.input_data])[0]
 
     def train(self):
 
         sess =  tf.Session()
+        self.sess = sess
+
         sess.run(tf.global_variables_initializer())
 
         # Learning
         for step in range(1000):
             # make batches of data and labels
             batch_xs, batch_ys = mnist.train.next_batch(100)
-            
+
             # Trainining step
             _ = sess.run([self.optimizer],
                          feed_dict={self.input_data  : batch_xs,
                                     self.input_labels: batch_ys})
-            
-            
+
+
         print('Training finished')
-        # Evaluate
-        for eps in np.linspace(0.001, 0.1, 20):
-            print('EPSILON', eps)
-
-            self.evaluate(mnist.test.images,
-                          mnist.test.labels,
-                          sess,
-                          eps=eps,
-                          num_test_images = 10)
-
-        sess.close()
 
 if __name__ == '__main__':
 
     # Import data as tensors
     mnist = input_data.read_data_sets("./MNIST_data/",
                                       one_hot=True)
-
     im = ImageClassifier(784, 10)
     im.train()
+
+
+    # grads  = im.get_gradient(mnist.test.images,
+    #                          mnist.test.labels)
+
+    j = Jack()
+    pirates = j.turn_em_into_a_pirate(mnist.test.images,
+                                      mnist.test.labels,
+                                      im,
+                                      eps=0.07,
+                                      num_test_images = 'all')
+
+    y_bad   = im.oracle(pirates)
+    y_pred  = im.oracle(mnist.test.images)
+    y_truth = [np.argmax(i) for i in mnist.test.labels]
+
+    print('Pred: {} Jacked: {}'.format(accuracy_score(y_truth, y_pred),
+                                       accuracy_score(y_truth, y_bad)))
+                                       
